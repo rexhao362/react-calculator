4e03d3c209063307e46e4f3be0adc11d28635d4b
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-01-16 21:46:12 +0000

    set-up and operators

---
+++ calculator/src/operators.js
@@ -0,0 +1,20 @@
+class Operators {
+    constructor(a, b) {
+        this.a = a
+        this.b = b
+    }
+
+    add = () => this.a + this.b
+
+    sub = () => this.a - this.b
+
+    mul = () => this.a * this.b
+
+    div = () => this.a / this.b
+
+    pow = () => Math.pow(this.a, this.b)
+
+    pi = () => Math.PI
+
+    
+}
